347807929d981a2505779eba329100533b7caeb0
Isel-A48278 <a48278@alunos.isel.pt>
2026-03-16 18:53:47 +0000

    16/03/2026 - Testing JGIT behavior + Status/Diff features
    Not quite sure how to translate data for readability yet...
    
    BTW we need to think about how to handle the cloned repos (when to keep, when to delete).
    Cloning takes time so we probably don't want to clone every time.
    But we also don't want to keep all repos as it takes LOTS of space.

--- backend/src/main/kotlin/pt/isel/service/gitCommunication.kt
+++ backend/src/main/kotlin/pt/isel/service/gitCommunication.kt
@@ -1,6 +1,13 @@
 package pt.isel.service
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.Status
+import org.eclipse.jgit.diff.DiffEntry
+import org.eclipse.jgit.diff.DiffFormatter
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.treewalk.AbstractTreeIterator
+import org.eclipse.jgit.treewalk.CanonicalTreeParser
+import java.io.ByteArrayOutputStream
 import java.io.File
 
 
@@ -35,6 +42,47 @@ fun getOrCloneGitRepo(repoURI: String, repoPathFile: File): Git {
     }
 }
 
+/**Temporary function for testing JGIT behavior, returns gitRepo Status.**/
+/*
+fun getGitRepoStatus(repo: Git): Status {
+    return repo.status().call()
+}*/
+
+/**Temporary function for testing JGIT behavior, returns a list of n commits**/
+fun getGitCommitLogs(repo: Git, maxCommits: Int): List<RevCommit> {
+    return repo.log().setMaxCount(maxCommits).call().toList()
+}
+
+/**Temporary function for testing JGIT behavior, returns a list of diffs**/
+fun getGitDiff(repo: Git, oldCommit: RevCommit, newCommit: RevCommit): List<DiffEntry>{
+    return repo.diff()
+        .setOldTree(getTreeParser(repo, oldCommit))
+        .setNewTree(getTreeParser(repo, newCommit))
+        .call()
+}
+
+/**Temporary function for testing JGIT behavior, creates a readable complete diff (akin to git terminal command)**/
+fun formatDiff(repo: Git, diffEntries: List<DiffEntry>): List<String>{
+    return diffEntries.map{ formatDiffEntry(repo, it) }
+}
+
+/**Temporary function for testing JGIT behavior, creates a readable diff entry (akin to git terminal command)**/
+fun formatDiffEntry(repo: Git, diffEntry: DiffEntry): String {
+    val out = ByteArrayOutputStream()
+    val df = DiffFormatter(out)
+    df.setRepository(repo.repository)
+    df.format(diffEntry)
+    return out.toString()
+}
+
+/**Temporary function for testing JGIT behavior, creates TreeIterator from Commit**/
+fun getTreeParser(repo: Git, commit: RevCommit): AbstractTreeIterator {
+    val reader = repo.repository.newObjectReader()
+    val parser = CanonicalTreeParser()
+    parser.reset(reader, commit.tree.id)
+    return parser
+}
+
 /**Temporary function for testing JGIT behavior, creates directory path from git repo URI**/
 fun getRepoPath(repoURI: String): String{
     val splitPath = repoURI.split("/").drop(2)
@@ -51,12 +99,27 @@ fun getRepoFile(repoPath: String): File{
 }
 
 const val reposStorageLocation = "gitRepos"
-const val testRepoURI = "https://github.com/octocat/Hello-World"
+const val testRepoURISmall = "https://github.com/octocat/Hello-World"
+const val testRepoURILarge = "https://github.com/githubtraining/hellogitworld"
+const val testRepoURI = testRepoURILarge
 
 fun main(){
     val repoFile = getRepoFile(getRepoPath(testRepoURI))
     //val clonedRepo = cloneGitRepo(testRepoURI, repoFile)
     //val existingRepo = getGitRepo(repoFile)
 
-    getOrCloneGitRepo(testRepoURI, repoFile)
+    val gitRepo = getOrCloneGitRepo(testRepoURI, repoFile)
+    //val status = getGitRepoStatus(gitRepo)
+
+    val commits = getGitCommitLogs(gitRepo, 5)
+
+    if(commits.isNotEmpty()){
+        val testCommit = commits.first()
+        if(testCommit.parentCount != 0){
+            val firstParent = testCommit.getParent(0)
+            val gitDiff = getGitDiff(gitRepo, firstParent, testCommit)
+            val formatedDiff = formatDiff(gitRepo, gitDiff)
+            println(formatedDiff.forEach{ println(it) })
+        }
+    }
 }
